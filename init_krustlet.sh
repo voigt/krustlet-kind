cfaa1aca0b518d56ebafdc3b09a89dac8df14b14
Christoph Voigt <voigt.christoph@gmail.com>
2021-11-02 14:17:08 +0100

    fix init logic

--- init_krustlet.sh
+++ init_krustlet.sh
@@ -1,23 +1,33 @@
-#!/bin/sh
+#!/bin/bash
+
+set -x
 
 kind create cluster --config=examples/kind.yaml --name=kruste
 
-echo "Started Kind..."
+echo "✅ Started Kind..."
 
 sleep 5
 
-kubectl -n kube-system patch ds kindnet -p '{"spec":{"template":{"spec":{"tolerations":[{"effect":"NoSchedule","key":"node-role.kubernetes.io/master","operator":"Exists"}]}}}}'
-kubectl -n kube-system patch ds kube-proxy -p '{"spec":{"template":{"spec":{"tolerations":[{"effect":"NoSchedule","key":"node-role.kubernetes.io/master","operator":"Exists"}]}}}}'
-echo "Patched DS..."
 
 # TODO check why error
 # ./start_krustlet_kind.sh: line 13: [: too many arguments
 echo "Waiting for csr..."
-until [ $(kubectl get csr kruste-worker2-tls 2>&1 > /dev/null) $? -eq 0 ]
+# echo $(kubectl get csr kruste-worker2-tls 2>&1 > /dev/null) $?
+# echo $?
+# until [ $(kubectl get csr kruste-worker2-tls 2>&1 > /dev/null) $? -eq 0 ]
+until $(kubectl get csr kruste-worker2-tls &> /dev/null)
 do
     sleep 1
 done
+# until [ $(kubectl get csr kruste-worker2-tls 2>&1 > /dev/null) $? -eq 0 ]
+# do
+#     sleep 1
+# done
 
 kubectl get csr kruste-worker2-tls
 kubectl certificate approve kruste-worker2-tls
-echo "Approed Krustlet..."
+echo "🦀 Approved Krustlet..."
+
+kubectl -n kube-system patch ds kindnet -p '{"spec":{"template":{"spec":{"tolerations":[{"effect":"NoSchedule","key":"node-role.kubernetes.io/master","operator":"Exists"}]}}}}'
+kubectl -n kube-system patch ds kube-proxy -p '{"spec":{"template":{"spec":{"tolerations":[{"effect":"NoSchedule","key":"node-role.kubernetes.io/master","operator":"Exists"}]}}}}'
+echo "Patched DS & Proxy..."
